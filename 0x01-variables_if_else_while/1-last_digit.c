58a588cbcb6fd78f8779c0a2799f371e278aefe6
codedibia <b.nwaruh@gmail.com>
2022-12-12 12:07:29 -0800

    Logical operators corrected

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ else if (last == 0)
     /* Print the last digit of n is 0 */
 printf("Last digit of %d is %d and is 0\n", n, last);
 }
-else if ((last == 6) && (last != 0))
+else if ((last < 6) && (last != 0))
 {
     /* Print the last digit of n is 6 not 0 */
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
