9081fd5a4b56147f8625c04290ec4fb302587c01
codedibia <b.nwaruh@gmail.com>
2022-12-13 15:35:19 -0800

    Description fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet, in lowercase
+ * print_alphabet - prints the alphabet, in lowercase
  *
  * Description: Prints the alphabet in lowercase
  *
