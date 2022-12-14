ac5e91739a2292e59790886a742084b0f7c736d7
codedibia <b.nwaruh@gmail.com>
2022-12-13 18:58:23 -0800

    Do remaining tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs
+ *
+ * @n: integer to be made absolute
+ *
+ * Return : int or int multiplied by negative
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	return (n * -1);
+}
