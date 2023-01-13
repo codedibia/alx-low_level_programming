540ccd6b5a252363662d533ba8e4444aff7e55ec
codedibia <b.nwaruh@gmail.com>
2023-01-13 03:21:32 -0800

    corrections

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,14 +12,14 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	if (argc <= 2 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		printf("Result = %d\n", atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
-	return (0);
 }
